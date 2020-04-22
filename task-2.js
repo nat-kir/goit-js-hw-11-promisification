539ddbfedc804251b5829e6193790d506bc93c69
Natalia Kiriunina <kiriunina.natalia@gmail.com>
2020-04-22 21:01:39 +0300

    goit-js-hw11-promisification

--- task-2.js
+++ task-2.js
@@ -1,28 +1,24 @@
 const users = [
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
-  ];
-  
-  const toggleUserState = (allUsers, userName, callback) => {
-    const updatedUsers = allUsers.map(user =>
-      user.name === userName ? { ...user, active: !user.active } : user,
-    );
-  
-    callback(updatedUsers);
-  };
-  
-  const logger = updatedUsers => console.table(updatedUsers);
-  
-  /*
-   * Сейчас работает так
-   */
-  toggleUserState(users, 'Mango', logger);
-  toggleUserState(users, 'Lux', logger);
-  
-  /*
-   * Должно работать так
-   */
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const updatedUsers = allUsers.map((user) => {
+        if (user.name === userName) {
+          user.active = !user.active;
+          resolve(users);
+        }
+      });
+    }, 1000);
+  });
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
